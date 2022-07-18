c038d9c45362e1b90e4e1ae82d02eb840251f210
amelia-laborte <93796860+amelia-laborte@users.noreply.github.com>
2022-07-17 20:22:00 -1000

    My first react app

---
+++ firstJsx/first-jsx/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <li>Learn react</li>
+      <li>Live react</li>
+      <li>Breath react</li>
+      <li>Bleed react</li>
+
+    </div>
+  );
+}
+
+export default App;
