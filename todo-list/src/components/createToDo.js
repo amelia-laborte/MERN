b060bd1ae7ad22895f4f9d9078faa2e386502567
amelia-laborte <93796860+amelia-laborte@users.noreply.github.com>
2022-07-28 09:32:26 -1000

    attempting to create another todo list

--- todo-list/src/components/createToDo.js
+++ todo-list/src/components/createToDo.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 
 const Todo = (props) => {
-  //   const [todoItemText, setTodoItemText] = useState("");
-  const mapTodoArrToListItems = (inputText, index) =>
-  <li key={index}>{inputText}</li>;
+
+  const mapTodoArrToListItems = (inputText, id) =>
+    
+  <li key={id}>{inputText}</li>;
 
   const [todoArr, setTodoArr] = useState([]);
 
   const createTodo = (submitEvent) => {
     submitEvent.preventDefault();
     console.log(submitEvent);
-    const inputText = document.getElementById("todoItemTextId").value;
+    const inputText = document.getElementById("id").value;
+    
     // setTodoItemText(inputText);
     setTodoArr([inputText, ...todoArr]);
 
@@ -20,7 +22,7 @@ const Todo = (props) => {
     <div>
       <h1>Things I need to do:</h1>
       <form onSubmit={createTodo}>
-        <input type="text" name="todoItemText" id="todoItemTextId"></input>
+        <input type="text" name="todoItemText" id="id"></input>
         <button type="submit">Add this to my list...</button>
       </form>
       <h2>My Todo List</h2>
